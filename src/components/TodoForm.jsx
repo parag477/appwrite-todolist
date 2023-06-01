fe8df543f7460b913ad1111c22c890cab23fa728
Parag <paragagrawal477@gmail.com>
2023-06-01 17:02:13 +0530

    TodoForm Completed👍

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { databases } from '../appwrite/appwriteConfig'
+
 
 function TodoForm() {
+
+  const [todo, setTodo] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const promise = databases.createDocument("64770c977d0d63a2466c", uuidv4(), {
+      todo
+    })
+    promise.then(
+      function(response) {
+        console.log(response);
+      },
+      function(error) {
+        console.log(error);
+      }
+    )
+    // window.location.reload()
+    e.target.reset();
+  }
+
   return (
-    <div>
-      
+    <div className="max-w-7xl mx-auto mt-10">
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex justify-center mb-10"
+      >
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter Todo"
+          className="border p-2 w-2/3 rounded-md"
+          onChange={(e) => {
+            setTodo(e.target.value)
+          }}
+        />
+        <button
+          className="bg-purple-500 p-2 text-white ml-2 rounded-md"
+          type="submit"
+        >
+          Add Todo
+        </button>
+      </form>
     </div>
   )
 }
